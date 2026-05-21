76ee1913ac441b3fa7cf9cdcfc10d29efe60115d
816 <816hs@816noMacBook-Pro.local>
2026-05-21 15:24:08 +0900

    SQLite追加

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello")
+import sqlite3
+conn = sqlite3.connect("app.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS study_records (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    study_date TEXT,
+    content TEXT,
+    hours INTEGER
+)
+""")
+
+cursor.execute("""
+INSERT INTO study_records
+(study_date, content, hours)
+VALUES (?, ?, ?)
+""", ("2026-05-21", "Git学習", 2))
+
+conn.commit()
+
+cursor.execute("""
+SELECT * FROM study_records
+""")
+
+records = cursor.fetchall()
+
+for record in records:
+    print(record)
+
+print("完了")
